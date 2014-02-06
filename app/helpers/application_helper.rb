fb719c0ec7f2582261365bbf56ebb3d3d117c4cc
David Brooks <davidbrooks.now@gmail.com>
2014-02-06 18:59:25 -0500

    fixed %.2f

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def format_price(cents)
  	 price_in_dollars = cents.to_f / 100
- 	 sprintf("2%.2f", price_in_dollars)
+ 	 sprintf("%.2f", price_in_dollars)
  	end
 
 end
